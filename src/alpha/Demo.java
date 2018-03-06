728dac7a4a2fd1a15561e668d7ae17f2179c1161
unknown <indianic@ind496.inic.local>
2018-03-06 11:09:29 +0530

    This is Log4j Tutorials Project

---
+++ src/alpha/Demo.java
@@ -0,0 +1,22 @@
+package alpha;
+
+import org.apache.logging.log4j.*;
+
+public class Demo {
+	
+	private static Logger log = LogManager.getLogger(Demo.class.getName());
+
+	public static void main(String[] args) {
+
+		log.debug("I am debugging");
+		
+		log.info("Object is present");
+		
+		log.error("Object is not present");
+		
+		log.fatal("this is fatel");
+		
+		
+	}
+
+}
---
+++ src/beta/Demo1.java
@@ -0,0 +1,20 @@
+package beta;
+
+import org.apache.logging.log4j.*;
+
+public class Demo1 {
+	
+	private static Logger log = LogManager.getLogger(Demo1.class.getName());
+
+	public static void main(String[] args) {
+
+		log.debug("I have to clicking on button");
+		
+		log.info("Button is displayed");
+		
+		log.error("Button is not displayed");
+		
+		log.fatal("Button is missing");
+	}
+
+}
